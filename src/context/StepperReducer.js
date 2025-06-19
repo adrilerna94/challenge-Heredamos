57b05815a0fe21b11a1ef0f015f40a8a6ff6c180
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-06-20 01:47:40 +0200

    feat: :white_check_mark: stepperInitialState done

---
+++ src/context/StepperReducer.js
@@ -0,0 +1,9 @@
+export const stepperInitialState =  {
+    current: 0,
+    steps: [
+        'Subir documentos',
+        'Revisión Legal',
+        'Firma heredero',
+        'Firma notario',
+    ],
+}
