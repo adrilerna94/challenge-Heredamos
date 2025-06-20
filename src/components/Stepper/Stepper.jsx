e65adf6d1ebdfc65d6e583a83bdcb3eedb9cda5d
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-06-20 03:05:40 +0200

    docs: :memo: imports in Stepper

---
+++ src/components/Stepper/Stepper.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+import CompletionModal from '../Modals/CompletionModal'
+import { useState } from 'react'
+import { useStepper } from '@/context/StepperContext';
+import StepButton from './StepButton';
+import StepControls from './StepControls';
+import { useRouter } from 'next/navigation';
+
+
