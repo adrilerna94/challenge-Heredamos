e5f6ff3508575a0cdac3de49cbd696c002acb83f
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-06-20 03:13:19 +0200

    feat: :white_check_mark: handleRestart, handleNext, navigateToHome functions

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -7,4 +7,27 @@ import StepButton from './StepButton';
 import StepControls from './StepControls';
 import { useRouter } from 'next/navigation';
 
+export default function Stepper() {
+    const { state, nextStep, prevStep, goToStep, reset} = useStepper();
+    const { steps, current } = state;
+    const [showModal , setShowModal] = useState(false);
+    const [isFinished, setIsFinished] = useState(false);
+    const router = useRouter();
 
+    const navigateToHome = () => router.push('/');
+
+    const handleRestart = () => {
+        reset();
+        setIsFinished(false);
+        setShowModal(false);
+    }
+
+    const handleNext = () => {
+        if (current < steps.length - 1) {
+            nextStep();
+        } else {
+            setIsFinished(true);
+            setShowModal(true);
+        }
+    }
+}
