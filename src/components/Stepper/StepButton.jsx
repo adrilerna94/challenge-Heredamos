8976098395d51d885729fb412d79e8fb8d25a855
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-06-20 02:41:45 +0200

    feat: :art: created Button with styles that change within the state

--- src/components/Stepper/StepButton.jsx
+++ src/components/Stepper/StepButton.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui';
 import { CheckCircle, checkCircle, Circle, PlayCircle } from 'lucide-react';
 
 export default function StepButton({ label, index, current, onClick }) {
@@ -11,6 +12,7 @@ export default function StepButton({ label, index, current, onClick }) {
         status = 'pending';
     }
 
+    // utilizamos formato objeto para evitar hadoken pattern (if elseif anidados)
     const iconMap = {
         done: <CheckCircle className="w-5 h-5 text-green-500"/>,
         active: <PlayCircle className="w-5 h-5 text-blue-500" />,
@@ -30,4 +32,19 @@ export default function StepButton({ label, index, current, onClick }) {
     };
 
     const baseStyles = 'flex justify-between items-center px-5 py-3 rounded-xl w-full text-left shadow-sm transition hover:shadow-md';
+
+    return (
+        <Button
+            disabled= {status === 'pending'}
+            onClick={() => status !== 'pending' && onClick(index)}
+            className={`${baseStyles} ${stateStylesMap[status]}`}
+        
+        >
+            <div className='flex items-center gap-3'>
+                {iconMap[status]}
+                <span className="text-base">{label}</span>
+            </div>
+            <span className={`text-sm font-medium ${textColorMap[status]}`} />
+        </Button>
+    );
 }
