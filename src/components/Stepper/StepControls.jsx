2a1a51ad81d3ba3c58b1dbb830b537812a6e5416
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-06-20 04:25:11 +0200

    fix: :bug: esconder Botón atras en la primera seccion del flujo

--- src/components/Stepper/StepControls.jsx
+++ src/components/Stepper/StepControls.jsx
@@ -1,14 +1,18 @@
 import { Button } from "@/components/ui";
 
-export default function StepControls({ onNext, onPrev, onlyBack = false, isLastStep = false }) {
+export default function StepControls({ isFirstStep = false, onNext, onPrev, onlyBack = false, isLastStep = false }) {
     return (
         <div className="flex gap-2 mt-4">
-            <Button
-                onClick = {onPrev}
-                className = "bg-white border border-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-100"
-            >
+            {/* solo mostramos botón atrás cuando esté en un paso mayor al primero */}
+            {!isFirstStep && (
+                <Button
+                    onClick = {onPrev}
+                    className = "bg-white border border-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-100"
+                >
                 Atrás
-            </Button>
+                </Button>
+            )}
+            
             {/* Solo mostramos botón siguiente si estamos como mínimo un paso anterior al final del proceso */}
             {!onlyBack && (
                 <Button
