c9917b6a11fc73a998e7e3772f8c62578a4416f7
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-06-20 02:18:23 +0200

    feat: :white_check_mark: StepperContext done

--- src/context/StepperContext.js
+++ src/context/StepperContext.js
@@ -6,5 +6,24 @@ const StepperContext = createContext();
 
 export const StepperProvider = ({ children }) => {
     const [state, dispatch] = useReducer(stepperReducer, stepperInitialState);
-    
+
+    const nextStep = () => {
+        if (state.current < state.steps.length - 1){
+            dispatch({type: 'NEXT'})
+        } else {
+            setShowModal(true);
+        }
+    }
+
+    const prevStep = () => dispatch({ type: 'PREV '});
+    const goToStep = (index) => dispatch({ type: 'SET' , payload: { index } });
+    const reset = () => dispatch({ type: 'RESET' });
+
+    return (
+        <StepperContext.Provider value= {{ state, nextStep, prevStep, goToStep, reset }}>
+            {children}
+        </StepperContext.Provider>
+    )
 }
+
+export const useStepper = () => useContext(StepperContext)
